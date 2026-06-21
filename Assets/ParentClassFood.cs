d2e0f63b6d10d24a0320df0841272189499298d4
monke984 <jamie.reade88@gmail.com>
2026-06-21 15:18:43 +1000

    Edited the parent class to make it easier to use, added a line of code that removes the food after consumption.

--- Assets/ParentClassFood.cs
+++ Assets/ParentClassFood.cs
@@ -2,21 +2,15 @@ using UnityEngine;
 
 public class ParentClassFood : MonoBehaviour
 {
-    public string Name { get; set; }
-    public int HealthImpact { get; set; }
-    public int Calories { get; set; }
-    public int HappinessImpact { get; set; }
-
-    public ParentClassFood(string name, int healthImpact, int calories, int happinessImpact)
-    {
-        Name = name;
-        HealthImpact = healthImpact;
-        Calories = calories;
-        HappinessImpact = happinessImpact;
-    }
+    public string Name;
+    public int HealthImpact ;    
+    public int Calories;
+    public int HappinessImpact;
 
     public virtual void Eat()
     {
         Debug.Log($"You eat {Name}, and it contains {Calories} calories, it impacts your health by {HealthImpact} and your happiness by {HappinessImpact}.");
+
+        Destroy(gameObject);
     }
 }
