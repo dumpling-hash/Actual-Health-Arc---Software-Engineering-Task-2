6c73b000340e8900800ca61eb79de02c2a8322de
monke984 <jamie.reade88@gmail.com>
2026-06-21 14:57:45 +1000

    I developed the scripts for the unhealthy food child class, adding its specific stat impacts, and the message in console when it is eaten.

---
+++ Assets/UnhealthyFoodChildClass.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UnhealthyFoodChildClass : ParentClassFood
+{
+    public int healthImpact = -10;
+    public int happinessImpact = 15;
+    public int calories = 10;
+
+    public void EatUnhealthyFood()
+    {
+        Debug.Log($"You eat unhealthy food, your health is now {healthImpact}, your happiness is {happinessImpact}, and you ate {calories} calories of food.");
+    }
+}
