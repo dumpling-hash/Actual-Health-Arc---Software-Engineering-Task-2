330e45df017fc027f5f3ad898cded818b667032f
dumplingface999 <jai.helm2009@gmail.com>
2026-06-21 15:51:52 +1000

    Sushi Photos
    Added a UI for my sushi, holds a good 2d pixel design just for the aesthetics.

--- Assets/MediumFood.cs
+++ Assets/MediumFood.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class MediumFood
+public class MediumFood : ParentClassFood
 {
+    void Start()
+    {
+        Name = "Ramen";
+        HealthImpact = 5;
+        Calories = 0;
+        HappinessImpact = 5;
+    }
     
+    private void OnTriggerEnter2D(Collider2D collision) // When 2d objects collides with the food object, it will check if object is the player and apply effects
+    {
+        if (collision.CompareTag("Player")) // Check if the colliding object has the tag "Player"
+        {
+            Player1 player = collision.GetComponent<Player1>();
+
+            if (player != null) // If the player component is found, apply the health, weight, and happiness impacts
+            {
+                player.health += HealthImpact;
+                player.weight += Calories;
+                player.happiness += HappinessImpact;
+            }
+            Eat();
+        }
+    }
 }
