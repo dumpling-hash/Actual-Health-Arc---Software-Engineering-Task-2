4004090f04e736ae58379f6bf7efe7885dfeb821
dumplingface999 <jai.helm2009@gmail.com>
2026-06-21 15:13:21 +1000

    Healthy Food Script
    Created a healthy food script, including the effects of eating healthy foods of plus 10 in health bar, and minus 10 for both the weight and happiness bars. Also added the colliding effects, if the food and 2D obejct collides, and then it will apply the effects of healthy foods.

---
+++ Assets/HealthyFood.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class HealthyFood : ParentClassFood
+{
+    void Start()
+    {
+        Name = "Sushi";
+        HealthImpact = 10;
+        Calories = -10;
+        HappinessImpact = -10;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision) // When 2d objects collides with the food object, it will check if object is the player and apply effects
+    {
+        if (collision.CompareTag("Player")) // Check if the colliding object has the tag "Player"
+        {
+            Player1 player = collision.GetComponent<Player1>();
+
+            if (player != null) // If the player component is found, apply the health, weight, and happiness impacts
+            {
+                player.health += HealthImpact;
+                player.weight += Calories;
+                player.happiness += HappinessImpact;
+            }
+            Eat();
+        }
+    }
+}
