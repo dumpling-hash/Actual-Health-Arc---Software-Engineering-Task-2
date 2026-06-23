a257d050a2ac6a2f24807aa5a650d8388d87c090
monke984 <jamie.reade88@gmail.com>
2026-06-23 14:26:29 +1000

    Debugging unhealthy food
    The unhealthy food class was not working as intended, due to its differences to my partners child classes. I adjusted to code to more closely match the other child classes, allowing the unhealthy food to affect the players stats as intended.

--- Assets/UnhealthyFoodChildClass.cs
+++ Assets/UnhealthyFoodChildClass.cs
@@ -9,19 +9,22 @@ public class UnhealthyFoodChildClass : ParentClassFood
             WeightImpact = 10;
             HappinessImpact = 15;
         }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
+            Player1 player = collision.GetComponent<Player1>();
+            if (player != null)
+            {
+                player.health += HealthImpact;
+                player.weight += WeightImpact;
+                player.happiness += HappinessImpact;
+            }
             Eat();
         }
     }
     public override void Eat()
     {
-        HealthImpact = -10;
-        HappinessImpact = 15;
-        WeightImpact = 10;
-
         Debug.Log($"You eat unhealthy food, your health is now {HealthImpact}, your happiness is {HappinessImpact}, and you gained {WeightImpact} weight.");
         base.Eat();
     }
