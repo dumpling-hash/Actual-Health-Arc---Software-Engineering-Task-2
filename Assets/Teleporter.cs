e1f6e0d343bd3771084be226164d210838281ac5
monke984 <jamie.reade88@gmail.com>
2026-06-24 19:21:48 +1000

    Redid Comments for Teleporter
    Re-typed comments on the teleporter script, due to issues surrounding the push/pull mechanism, which causes a conflict in scripts, leading to me having to rewrite the comments.

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour // Sets teleporter class and allows it to be assigned to objects
 {
-    public Transform destination;
+    public Transform destination; // Sets the location the player will be teleported to
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) // Triggers the teleportation function when the player collides with object 
     {
         if (other.CompareTag("Player")) // Ensures the object collides with player
         {
             other.transform.position = new Vector2(destination.position.x, destination.position.y);
-            Debug.Log("Welcome to a new Continent!");
+            // Teleports the player to set destination, throough x and y coodinates
+            Debug.Log("Welcome to a new Continent!"); // Prints the welcome message in the console after the player is teleported to a different continent level
         }
     }
 }
